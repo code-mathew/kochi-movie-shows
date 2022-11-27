a7c274cc7301a530f9c26396ac3285b7ef9ad54b
iammathew <mathewjosemail@gmail.com>
2022-11-27 19:43:25 +0530

    Sun, Nov 27, 2022  7:43:25 PM

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://in.bookmyshow.com/explore/movies-kochi');
 
-     await page.keyboard.down("PageDown");
-     await page.keyboard.press("PageDown");
+     //await page.keyboard.down("PageDown");
+     for(let i=0;i<7;i++){  // loops the pagedown btn 
+        await page.keyboard.press("PageDown");
+     }
+     await page.waitForSelector('.jBFgAs .jaypsW', { /// waits for the selector to become hidden
+        hidden:true,
+        // visible:false
+      });
+     for(let i=0;i<7;i++){  // loops the pagedown btn //gives some more time to fetch
+        await page.keyboard.press("PageDown");
+     }
+     
+     
+     
      await page.screenshot({path:'example.png', fullPage:true });
     //Running Movies section
-     const runningMovies = await page.evaluate(()=> 
+     const runningMoviesAlpha = await page.evaluate(()=> 
      Array.from(document.querySelectorAll('.jBFgAs .gteFjS .cVsUJT .gDSiod a'), (e)=> ({
          title: e.querySelector('.Xdzak .kKvMMQ .dmACKf .gyGThn .cFdPHn').innerText,
          cert: e.querySelector('.Xdzak .kKvMMQ .dmACKf .gyGThn .fbwUfl').innerText,
@@ -23,7 +35,22 @@ const puppeteer = require('puppeteer');
      }))
      );
 
-     console.log(runningMovies);
+     //Balance movie list
+     const runningMoviesBeta = await page.evaluate(()=>
+     Array.from(document.querySelectorAll('.jBFgAs .gqBECX .iIGMqX .iUuHNJ a'), (e)=> ({
+        title: e.querySelector('.eQcIov .cWbeuJ .WfspT .cBsijw').innerText,
+        cert: e.querySelector('.eQcIov .cWbeuJ .WfspT .bMPkUy').innerText,
+        lang: e.querySelector('.eQcIov .cWbeuJ .WfspT:nth-child(3) .bMPkUy').innerText,
+        image: e.querySelector('.eQcIov img').src,
+        url:e.href
+    })));
+
+    
+     console.log(runningMoviesAlpha);
+     console.log("Balance");
+     console.log(runningMoviesBeta);
+
+    const runningMovies = runningMoviesAlpha.concat(runningMoviesBeta);
 
     //Save data to JSON file
 
